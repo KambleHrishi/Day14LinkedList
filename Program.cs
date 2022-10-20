2a1e2bc5c130ad4ffa084472bed85ddbc8591019
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-20 21:05:43 +0530

    append added

--- Program.cs
+++ Program.cs
@@ -3,17 +3,23 @@
     public class Program
     {
         public static void Main(string[] args)
-        {
-            Console.WriteLine("LinkedList Problem");
+        {  
             LinkedList list = new LinkedList();
             //list.Add(56);
             //list.Add(30);
             //list.Add(70);
             //list.Display();
-            list.Add(70);
-            list.Add(30);
-            list.Add(56);
+
+            //list.Add(70);
+            //list.Add(30);
+            //list.Add(56);
+            //list.Display();
+
+            list.Append(56);
+            list.Append(30);
+            list.Append(70);
             list.Display();
+
         }
     }
 }
