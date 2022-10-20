fc8d0792a786be70b46ac5b6daf988d9f7637f64
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-20 20:48:24 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day14LinkedList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList Problem");
+        }
+    }
+}
