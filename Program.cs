1a6a8e14637190c5766f8794550af4c31458db46
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-20 20:54:55 +0530

    created linked list

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to LinkedList Problem");
+            Console.WriteLine("LinkedList Problem");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
